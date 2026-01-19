8095778e132bfbc569bf7f49fef859868948f882
tdat25 <thanhdat.dv73@gmail.com>
2026-01-19 07:36:19 +0700

    uu

--- Web_04_a/src/java/model/UserDAO.java
+++ Web_04_a/src/java/model/UserDAO.java
@@ -5,6 +5,7 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -26,8 +27,9 @@ public class UserDAO {
             String sql = "SELECT * FROM tblUsers "
                     + " WHERE userID='" + username + "'";
             System.out.println(sql);
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, username);
+            ResultSet rs = pst.executeQuery(sql);
             
             UserDTO user = null;
             while (rs.next()) {
---
+++ Web_04_a/web/login.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : login
+    Created on : Jan 8, 2026, 11:08:44 AM
+    Author     : VO THANH DAT
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form action ="MainController" method ="post">
+            Username: <input type="text" name="txtUsername"/> <br/><!-- comment -->
+            Password: <input type="password" name="txtPassword"/> <br/><!-- comment -->
+            <input type="submit" value="Login"/>
+    </body>
+</html>
